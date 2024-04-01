3c6ee345efb10b6004777a9a183f0864b4bc8316
Lana Andrade <lanaandrade2003@gmail.com>
2024-03-31 21:35:36 -0300

    feat: repo criado

---
+++ src/components/Nav.jsx
@@ -0,0 +1,24 @@
+import {Link} from  'react-router-dom'
+
+function Nav() {
+  
+
+    return (
+      <>
+        <h1> AAAAAA </h1>
+        <header className='menu'>
+            <nav className='nav-menu'>
+                <div id="logo">
+                    <img src="../src/assets/"/>
+                </div>
+                <ul>
+
+                </ul>
+            </nav>
+        </header>
+      </>
+    )
+
+  }
+  
+  export default Nav
