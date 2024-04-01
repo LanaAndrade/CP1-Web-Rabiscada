51846bde397ec92d66d5fafaf9e05df9d623bc1f
Lana Andrade <lanaandrade2003@gmail.com>
2024-04-01 20:15:38 -0300

    feat: nav estilizada

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,27 +8,28 @@ function Nav() {
     return ( 
       <>
         <header className='menu'>
+
             <nav className='nav-menu'>
 
-              <ul>
+              <ul id="nav-menu-ul">
                 
-                <div id="logo">
-                    <img id="logo-img" src="./src/assets/2eTCKMTV4wtQuheCvh7pHonpqss-cropped.svg"/>
+                <img id="logo-img" src="./src/assets/2eTCKMTV4wtQuheCvh7pHonpqss-cropped.svg"/>
+                
+                <div id="links-container">
+                  <Link to="/" className='link'>
+                    Home
+                  </Link>{' '}
+                  {''}
+                  <Link to="/produtos" className='link'>
+                    Produtos
+                  </Link>
+                  <Link to="/sobre" className='link'>
+                    Sobre
+                  </Link>
+                  <Link to="/contato" className='link'>
+                    Contato
+                  </Link>
                 </div>
-                
-                <Link to="/" className='link'>
-                  Home
-                </Link>{' '}
-                {''}
-                <Link to="/produtos" className='link'>
-                  Produtos
-                </Link>
-                <Link to="/sobre" className='link'>
-                  Sobre
-                </Link>
-                <Link to="/contato" className='link'>
-                  Contato
-                </Link>
               </ul>
               
             </nav>
