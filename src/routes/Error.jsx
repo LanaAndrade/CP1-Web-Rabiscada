a4466dd5a8528792a852ff503a9a3b1ddac24175
CaioFreitasCF <caio.freitascfc@gmail.com>
2024-04-01 22:53:30 -0300

    feat: error feito

--- src/routes/Error.jsx
+++ src/routes/Error.jsx
@@ -6,6 +6,11 @@ function Error() {
 
     return (
       <>
+      <img id="logo-img" src="./src/assets/2eTCKMTV4wtQuheCvh7pHonpqss-cropped.svg"></img>
+
+      <h1> Erro - 404  </h1>
+      <h2>Página não encontrada </h2>
+      <h3>URL digitada não está disponível nos servidores do site</h3>
       </>
     );
   }
