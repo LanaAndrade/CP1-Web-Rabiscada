ab334ab4cdfdf3042f4e7fc79ecda5c5bf0cf268
Lana Andrade <lanaandrade2003@gmail.com>
2024-04-01 21:24:41 -0300

    feat: footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,30 @@ function Footer() {
 
     return (
       <>
+      <footer>
+        <div class="footer">
+
+          <div class="row">
+            <ul>
+              <li><a href="#">Caio Freitas</a></li>
+              <li><a href="#">Felipe Santana</a></li>
+              <li><a href="#">Lana Andrade</a></li>
+              <li><a href="#">Lucas Garcia</a></li>
+              <li><a href="#">Mateus Tibão</a></li>
+            </ul>
+          </div>
+
+          <div class="row">
+            Av. Paulista, 1106 - 7º andar - Bela Vista, São Paulo - SP, 01311-000
+          </div>
+
+
+          <div class="row">
+            Rabiscada © 2024 - FIAP 
+          </div>
+        </div>
+      </footer>
+
       </>
     )
   }
